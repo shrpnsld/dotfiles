f3178fd157617eb1011798cf8a9bcb66f6ca62d5
shrpnsld <anton.degtiar@gmail.com>
2024-03-01 00:37:45 +0200

    Some keymaps updates for Neovim

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -61,17 +61,24 @@ end
 
 vim.g.mapleader = " "
 
+-- Preventing these mappings to move between tabs
+vim.keymap.set("", "<Leader><Tab>", "", { silent = true })
+vim.keymap.set("", "<Leader><S-Tab>", "", { silent = true })
 -- Tabs
 vim.keymap.set("", "<Tab>", next_tab, { silent = true, desc = "Next tab" })
 vim.keymap.set("", "<S-Tab>", previous_tab, { silent = true, desc = "Previous tab" })
-vim.keymap.set("", "<Leader><Tab>", move_tab_right, { silent = true, desc = "Move tab right" })
-vim.keymap.set("", "<Leader><S-Tab>", move_tab_left, { silent = true, desc = "Move tab left" })
-vim.keymap.set("", "<Leader>`", "<Cmd>tabnew<CR>", { silent = true, desc = "New tab" })
+vim.keymap.set("", "<Leader><Tab>`", "<Cmd>tabnext #<CR>", { silent = true, desc = "Last opened tab" })
+vim.keymap.set("", "<Leader><Tab>l", move_tab_right, { silent = true, desc = "Move tab right" })
+vim.keymap.set("", "<Leader><Tab>h", move_tab_left, { silent = true, desc = "Move tab left" })
+vim.keymap.set("", "<Leader><Tab><Tab>", "<Cmd>tabnew<CR>", { silent = true, desc = "New tab behind current one" })
+vim.keymap.set("", "<Leader><Tab><S-Tab>", "<Cmd>-tabnew<CR>", { silent = true, desc = "New tab before current one" })
 
 -- Quickfix List
 vim.keymap.set("", "<Leader>x", toggle_quickfix_list, { silent = true, desc = "Toggle Quickfix List" })
 vim.keymap.set("", "<Leader>c", "<Cmd>cnext<CR>", { silent = true, desc = "Next in Quickfix List" })
 vim.keymap.set("", "<Leader>z", "<Cmd>cprevious<CR>", { silent = true, desc = "Previous in Quickfix List" })
+vim.keymap.set("", "<Leader>Z", "<Cmd>cfirst<CR>", { silent = true, desc = "First in Quickfix List" })
+vim.keymap.set("", "<Leader>C", "<Cmd>clast<CR>", { silent = true, desc = "Last in Quickfix List" })
 
 -- Buffers
 vim.keymap.set("", "<Leader>w", "<Cmd>bwipeout<CR>", { silent = true, desc = "Buffer wipeout" })
@@ -85,14 +92,27 @@ vim.keymap.set("", "<Leader>>", "<Cmd>Texplore<CR>", { silent = true, desc = "Ne
 -- Other Stuff
 vim.keymap.set("", "<Leader>/", "<Cmd>nohlsearch<CR>", { silent = true, desc = "Stop highlighting search" })
 vim.keymap.set("", "<Leader>m", "<Cmd>make!<CR>", { silent = true, desc = "Build project" })
+vim.keymap.set("", "<S-BS>", "<Cmd>pop<CR>", { silent = true, desc = "Jump to previous entry in tag stack" })
+vim.keymap.set("", "<S-Enter>", "<Cmd>tag<CR>", { silent = true, desc = "Jump to next entry in tag stack" })
+vim.keymap.set("", "<Leader>Q", "<Cmd>qa!<CR>", { silent = true, desc = "Just quit!" })
 
 -- Telescope
 vim.keymap.set("", "<Leader>f", telescope_find_files_in_project, { silent = true, desc = "Fuzzy find files" })
 vim.keymap.set("", "<Leader>F", telescope_find_files_any, { silent = true, desc = "Fuzzy find files, including 'build/' and 'external/' directories" })
 vim.keymap.set("", "<Leader>b", require("telescope.builtin").buffers, { silent = true, desc = "Live grep files" })
-vim.keymap.set("", "<Leader>g", require("telescope.builtin").live_grep, { silent = true, desc = "Live grep files" })
-vim.keymap.set("", "<Leader>t", require("telescope.builtin").current_buffer_fuzzy_find, { silent = true, desc = "Fuzzy find in current buffer" })
+vim.keymap.set("", "<Leader>g", require("telescope.builtin").current_buffer_fuzzy_find, { silent = true, desc = "Fuzzy find in current buffer" })
+vim.keymap.set("", "<Leader>G", require("telescope.builtin").live_grep, { silent = true, desc = "Live grep files" })
+vim.keymap.set("", "<Leader>!", require("telescope.builtin").diagnostics, { silent = true, desc = "Fuzzy find in diagnostics" })
 
+vim.keymap.set("", "<Leader><Tab>f", "<Cmd>tabnew | lua telescope_find_files_in_project()<CR>", { silent = true, desc = "Fuzzy find files in a tab behind current one" })
+vim.keymap.set("", "<Leader><Tab>F", "<Cmd>tabnew | lua telescope_find_files_any()<CR>", { silent = true, desc = "Fuzzy find files, including 'build/' and 'external/' directories in a tab behind current one" })
+vim.keymap.set("", "<Leader><Tab>b", "<Cmd>tabnew | lua require('telescope.builtin').buffers()<CR>", { silent = true, desc = "Live grep files in a tab behind current one" })
+vim.keymap.set("", "<Leader><Tab>G", "<Cmd>tabnew | lua require('telescope.builtin').live_grep()<CR>", { silent = true, desc = "Live grep files in a tab behind current one" })
+
+vim.keymap.set("", "<Leader><S-Tab>f", "<Cmd>-tabnew | lua telescope_find_files_in_project()<CR>", { silent = true, desc = "Fuzzy find files in a new tab before current one" })
+vim.keymap.set("", "<Leader><S-Tab>F", "<Cmd>-tabnew | lua telescope_find_files_any()<CR>", { silent = true, desc = "Fuzzy find files, including 'build/' and 'external/' directories in a new tab before current one" })
+vim.keymap.set("", "<Leader><S-Tab>b", "<Cmd>-tabnew | lua require('telescope.builtin').buffers()<CR>", { silent = true, desc = "Live grep files in a new tab before current one" })
+vim.keymap.set("", "<Leader><S-Tab>G", "<Cmd>-tabnew | lua require('telescope.builtin').live_grep()<CR>", { silent = true, desc = "Live grep files in a new tab before current one" })
 vim.keymap.set("", "<Leader>?", require("telescope.builtin").keymaps, { silent = true, desc = "Fuzzy find in keymaps" })
 vim.keymap.set("", "<Leader><Leader>", require("telescope.builtin").resume, { silent = true, desc = "Reopen last Telescope find" })
 
